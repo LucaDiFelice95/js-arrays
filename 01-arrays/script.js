632962bf312b45f0698b7385e45274932f4ab9d1
Luca Di Felice <lucadifelice95@gmail.com>
2026-05-19 11:24:19 +0200

    Exercise 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -2,20 +2,21 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers[3];
+// const fourthTeacher = teachers[3];
 // console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
-teachers[4] = "Patrick";
+// teachers[4] = "Patrick";
 // console.log(teachers[4]);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers.pop();
-console.log(teachers);
+// const lastTeacher = teachers.pop();
+// console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+// const firstTeacher = teachers.shift();
+// console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
