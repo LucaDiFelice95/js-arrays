0820b0f21be9e2475efd287dbd835a7bf7ebeec8
Luca Di Felice <lucadifelice95@gmail.com>
2026-05-19 11:30:35 +0200

    Exercise 7

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -30,7 +30,8 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+// const lewisIndex = teachers.indexOf("Lewis");
+// console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
